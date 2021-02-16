3a1f6ee84d7c8bab519e2108280aec272b918f78
vaibhav <codekul.vaibhav@gmail.com>
2021-02-16 13:58:13 +0530

    Nested try/catch Exception

--- src/constructor_this_super/Student.java
+++ src/constructor_this_super/Student.java
@@ -14,22 +14,21 @@ public class Student {
         System.out.println("In constructor");
     }
 
-    public Student(){
+    public Student() {
+
+    }
+
+    public static void main(String[] args) {
+        Student student = new Student(1, "Hari", "Pune");
+        Student student1 = new Student(1, "Hari", "Pune");
+        Student student2 = new Student(1, "Hari", "Pune");
+        Student student3 = new Student(1, "Hari", "Pune");
+        student.show();
 
     }
 
     private void show() {
         System.out.println("id->" + id + "name->" + name + "add->" + add);
-    }
-
-
-    public static void main(String[] args) {
-        Student student = new Student(1,"Hari","Pune");
-        Student student1 = new Student(1,"Hari","Pune");
-        Student student2 = new Student(1,"Hari","Pune");
-        Student student3 = new Student(1,"Hari","Pune");
-        student.show();
-
     }
 
 

d0ca027d0180cca2b4ba63d2b97027266417585c
vaibhav <codekul.vaibhav@gmail.com>
2021-02-24 13:39:07 +0530

    Thread methods

--- src/multithreading/LambadaDemo.java
+++ src/multithreading/LambadaDemo.java
@@ -1,9 +1,9 @@
 package multithreading;
 
-public class lambadaDemo {
+public class LambadaDemo {
 
     public static void main(String[] args) {
-        LambadaInterface s =(str1,str2)-> str1+str2;
+        LambadaInterface s =(str1,str2) -> str1+str2;
         System.out.println(s.concat("Pune","Mumbai"));
 
     }
@@ -12,5 +12,5 @@ public class lambadaDemo {
 @FunctionalInterface
 interface LambadaInterface{
 
-    public String concat(String a, String b);
+     String concat(String a, String b);
 }
--- src/multithreading/RunnableDemo.java
+++ src/multithreading/RunnableDemo.java
@@ -1,22 +1,22 @@
 package multithreading;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class RunnableDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread t1 = new Thread(() -> {
+            System.out.println(Thread.currentThread().isAlive());
             for (int i = 1; i <= 5; i++) {
                 System.out.println("Hii");
                 try {
                     Thread.sleep(500);
+
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-        });
-
+        },"T1");
+        
         Thread t2 = new Thread(() -> {
+            System.out.println(Thread.currentThread().isAlive());
             for (int i = 1; i <= 5; i++) {
                 System.out.println("Hello");
                 try {
@@ -25,10 +25,17 @@ public class RunnableDemo {
                     e.printStackTrace();
                 }
             }
-        });
+        },"t2");
+
         t1.start();
         t2.start();
 
+        t1.join();
+        t2.join();
+        System.out.println("Exit");
+
+        System.out.println(t1.isAlive());
+        System.out.println(t2.isAlive());
 
     }
 }
